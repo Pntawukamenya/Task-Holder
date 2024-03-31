cf8d56fdd6d6f679876f15cec86fc89801bda041
Pntawukamenya <p.ntawukame@alustudent.com>
2024-04-01 01:31:28 +0200

    Js not working??

--- js/main.js
+++ js/main.js
@@ -1,36 +1,39 @@
 let todoForm = document.querySelector(".todo-form form");
-let htmlBody = document.querySelector(".todo-table table tbody");
+let HtmlBody = document.querySelector("todo-table table tbody");
 
-todoForm.addEventListener('submit', function(e){
+todoForm.addEventListener("submit", function(e){
     e.preventDefault();
-   
+
     let todoTitle = todoForm.todoname.value;
 
-    addTodoItem(todoTitle);  
-    
-    todoForm.todoname.value = '';
+    addTodoItem(todoTitle);
+
+    todoForm.todoname.value = "";
 });
 
 htmlBody.addEventListener('click', function(e){
     if(e.target.getAttribute('type')=='checkbox'){
-        let tr = e.target.parentNode.parentNode;
+        let tr =e.target.parentNode.parentNode;
         if(e.target.checked){
-         tr.classlist.add('complete');
+            tr.classList.add('complete');
         }else{
-            tr.classlist.remove('complete');
+            tr.classList.remove('complete');
         }
-    }   
+    }
 });
 
 
 
 
-function addTodoItem(title){
-    let htmlContent = <tr>
-                           <td><input type="checkbox" name="" id=""></td>
-                           <td>Pack toiletry bag</td>
-                           <td>Pending</td>
-                        </tr>
-                        
+
+
+function addTodoItem(Title){
+    let htmlContent = `<tr>
+                        <td><input type="checkbox" name="" id=""></td>
+                        <td>${title}</td>
+                        <td>Pending</td>
+                    </tr>';
+
     htmlBody.innerHTML += htmlContent;
+
 }
