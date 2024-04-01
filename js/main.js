2920d6e7726336fca836eedf0c633d967e64ec03
Pntawukamenya <p.ntawukame@alustudent.com>
2024-04-01 02:33:58 +0200

    Deleted the whole js file and made a new one

--- js/main.js
+++ js/main.js
@@ -1,38 +1,58 @@
-let todoForm = document.querySelector(".todo-form form");
-let HtmlBody = document.querySelector("todo-table table tbody");
+document.querySelector("button").addEventListener("click", function() {
+    var text = document.querySelector("input").value;
+    var table = document.querySelector("table tbody");
+    var newRow = table.insertRow(table.rows.length);
+    var newCell = newRow.insertCell(0);
+    var newCheckbox = document.createElement("input");
+    newCheckbox.type = "checkbox";
+    newCell.appendChild(newCheckbox);
+    newCell = newRow.insertCell(1);
+    newCell.innerHTML = text;
+    document.querySelector("input").value = "";
+})
 
-todoForm.addEventListener("submit", function(e){
-    e.preventDefault();
-
-    let todoTitle = todoForm.todoname.value;
-
-    addTodoItem(todoTitle);
-
-    todoForm.todoname.value = "";
-});
-
-HtmlBody.addEventListener('click', function(e){
-    if(e.target.getAttribute('type')=='checkbox'){
-        let tr =e.target.parentNode.parentNode;
-        if(e.target.checked){
-            tr.classList.add('complete');
-        }else{
-            tr.classList.remove('complete');
-        }
+document.addEventListener("change", function(event) {
+    if (event.target.tagName === "INPUT" && event.target.checked) {
+        event.target.parentNode.parentNode.style.textDecoration = "line-through";
+    } else if (event.target.tagName === "INPUT" && !event.target.checked) {
+        event.target.parentNode.parentNode.style.textDecoration = "none";
     }
-});
+})
+
+document.addEventListener("click", function(event) {
+    if (event.target.tagName === "TD" && event.target.parentNode.cells[0].querySelector("input").checked) {
+        event.target.parentNode.parentNode.remove();
+    }
+})
+
+document.addEventListener("change", function(event) {
+    if (event.target.tagName === "INPUT" && event.target.checked) {
+        event.target.parentNode.parentNode.cells[2].innerHTML = "Complete";
+    } else if (event.target.tagName === "INPUT" && !event.target.checked) {
+        event.target.parentNode.parentNode.cells[2].innerHTML = "Pending";
+    }
+})
+
+
+document.querySelector("table").addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && event.target.tagName === "INPUT") {
+        var text = event.target.value;
+        var table = document.querySelector("table tbody");
+        var newRow = table.insertRow(table.rows.length);
+        var newCell = newRow.insertCell(0);
+        var newCheckbox = document.createElement("input");
+        newCheckbox.type = "checkbox";
+        newCell.appendChild(newCheckbox);
+        newCell = newRow.insertCell(1);
+        newCell.innerHTML = text;
+        event.target.value = "";
+        event.preventDefault();
+    }
+})
 
 
 
+document.querySelector("button").addEventListener("click", function() {
+    document.querySelector("input").click();
+})
 
-
-
-function addTodoItem(Title){
-    let htmlContent = `<tr>
-                        <td><input type="checkbox" name="" id=""></td>
-                        <td>${title}</td>
-                        <td>Pending</td>
-                    </tr>`;
-
-    htmlBody.innerHTML += htmlContent;
-};
